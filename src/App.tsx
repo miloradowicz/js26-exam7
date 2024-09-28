b312914018e6b7bad413b5021189575857f4d96d
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-09-28 16:04:57 +0600

    Layout test, App functional without interactive studd

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { OrderUnit, Product } from './types.d';
+import { products } from './data';
+import { useState } from 'react';
+import OrderList from './components/OrderList/OrderList';
+import ProductList from './components/ProductList/ProductList';
+import './App.css';
+
+const order: OrderUnit[] = [
+  { product: products[1], qty: 2 },
+  { product: products[3], qty: 4 },
+  { product: products[0], qty: 1 },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const addProduct = () => {};
+  const deleteProduct = () => {};
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container'>
+      <h1>Super Duper Restaurant</h1>
+      <div className='row row-cols-1 row-cols-md-2 flex-column-reverse flex-md-row'>
+        <OrderList order={order} onItemDeleteClick={deleteProduct}></OrderList>
+        <ProductList products={products} onItemClick={addProduct} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/data.ts
@@ -0,0 +1,12 @@
+import { Product, ProductType } from './types.d';
+
+export const products: Product[] = [
+  { id: 0, displayName: 'Hamburger', type: ProductType.Food, price: 80 },
+  { id: 1, displayName: 'Cheeseburger', type: ProductType.Food, price: 90 },
+  { id: 2, displayName: 'Fries', type: ProductType.Food, price: 45 },
+  { id: 3, displayName: 'Coffee', type: ProductType.Drink, price: 70 },
+  { id: 4, displayName: 'Tea', type: ProductType.Drink, price: 50 },
+  { id: 5, displayName: 'Cola', type: ProductType.Drink, price: 40 },
+];
+
+export const currency: string = 'KGS';
